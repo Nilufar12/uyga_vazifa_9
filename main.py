a40e768073eb1e04c4078c2f60862729c212c44e
Nilufar <nilufargapparova8@gmail.com>
2026-01-15 10:43:44 +0500

    5gacha darsni bajardim

---
+++ main.py
@@ -0,0 +1,48 @@
+#1. Sonlar ro‘yxati berilgan.
+# `map()` va `lambda` yordamida har bir sonni 3 ga ko‘paytiring.
+
+# l = [2, 6, 8, 6, 56, 21, 53, 57]
+
+# k = list(map(lambda x: x * 3, l))
+# print(k)
+
+# 2. Sonlar ro‘yxatidan `map()` orqali
+# har bir sonning kvadratini hisoblang.
+
+# def kv(i):
+#     return i * i
+#
+#
+# l = [2, 6, 8, 6, 56, 21, 53, 57]
+#
+# k = list(map(kv, l))
+# print(k)
+
+# 3. Berilgan satrlar ro‘yxatida
+# `map()` yordamida har bir satrni katta harflarga o‘tkazing.
+
+# words = ["ziyoda", "salom", "toxir", "python"]
+#
+# text = list(map(str.upper, words))
+# print(text)
+
+# 4. Ismlar ro‘yxatidan `map()` orqali
+# har bir ismning uzunligini hisoblang.
+
+# names = ["Ziyoda", "Diyora", "Ibrohim", "Jasur"]
+#
+# n = list(map(len, names))
+# print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
